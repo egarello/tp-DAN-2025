81df9cbe3e0f0dfe4c6c5015d2a465d0e0e05b1e
Juan Di Liscia <jfdiliscia2015@gmail.com>
2026-05-29 14:05:09 -0300

    captura y manejo de excepciones en gestion-svc

--- services/gestion-svc/src/main/java/edu/utn/frsf/isi/dan/gestion/model/Hotel.java
+++ services/gestion-svc/src/main/java/edu/utn/frsf/isi/dan/gestion/model/Hotel.java
@@ -5,8 +5,10 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import jakarta.persistence.*;
 import lombok.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "hotel", schema = "tp_dan")
@@ -18,13 +20,28 @@ public class Hotel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @NotBlank(message = "El nombre del hotel es obligatorio")
     private String nombre;
+
+    @NotBlank(message = "El CUIT del hotel es obligatorio")
     private String cuit;
+
+    @NotBlank(message = "El domicilio del hotel es obligatorio")
     private String domicilio;
+
+    @NotNull(message = "La latitud del hotel es obligatoria")
     private Double latitud;
+    @NotNull(message = "La longitud del hotel es obligatoria")
     private Double longitud;
+
+    @NotBlank(message = "El telefono del hotel es obligatorio")
     private String telefono;
+
+    @NotBlank(message = "El correo de contacto del hotel es obligatorio")
     private String correoContacto;
+
+    @NotNull(message = "La categoria del hotel es obligatoria")
     private Integer categoria;
     @Builder.Default
     private Boolean cerrado = false;

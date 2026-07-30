0dfa8059939581dcb8b46fb501849b21b9f49907
Juan Di Liscia <jfdiliscia2015@gmail.com>
2026-07-30 15:48:33 -0300

    dashboard actualizado

--- common/dan-eureka-server/src/main/java/edu/utn/frsf/isi/dan/eureka/DanEurekaServerApplication.java
+++ common/dan-eureka-server/src/main/java/edu/utn/frsf/isi/dan/eureka/DanEurekaServerApplication.java
@@ -1,7 +1,11 @@
 package edu.utn.frsf.isi.dan.eureka;
 
+import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
@@ -10,5 +14,15 @@ public class DanEurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DanEurekaServerApplication.class, args);
+    }
+
+    @Bean
+    MeterBinder eurekaRegistrySize(PeerAwareInstanceRegistry eurekaRegistry) {
+        return meterRegistry -> Gauge.builder(
+                        "eureka.server.registry.size",
+                        eurekaRegistry,
+                        registry -> registry.getApplications().size())
+                .description("Number of applications registered in Eureka")
+                .register(meterRegistry);
     }
 }
